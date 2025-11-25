6c62d4e58307fe5445186e52154d573b368f762b
LeggyBoiTim <105779117+LeggyBoiTim@users.noreply.github.com>
2025-11-25 15:41:32 +0100

    Added ParentComponent and ChildComponent

---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+import ChildComponent from './ChildComponent.vue';
+
+const message = 'Test';
+const sentMessage = ref('');
+
+const handleMessage = (newMessage) => {
+    sentMessage.value = newMessage;
+}
+</script>
+
+
+
+<template>
+    <ChildComponent :message="message" @update-message="handleMessage" />
+    <p>Bericht: {{ sentMessage }}</p>
+</template>
